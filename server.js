4a69c73449af33cf5bd8b14e09ee7bf7d8f29946
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-15 11:34:43 +0530

    g

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const app = express();
+const app = express(); // server ka instance create kra hae 
 
 app.get("/", (req, res) => {
 
@@ -12,5 +12,5 @@ app.get("/about", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000");
-});
+    console.log(`server is running on port 3000`); //server ko start krne kae liye 3000 is the port number 
+}); 
