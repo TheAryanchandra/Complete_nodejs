0a18da78925152d74f1b68f4948beee240a2ff1d
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-17 23:50:13 +0530

    cj

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const express = require('express');
+// server ko start krna 
 
-const app = express(); // server ka instance create kra hae 
-
-app.get("/", (req, res) => {
-
-    res.send("hello world");
-});
-
-app.get("/about", (req, res) => {
-    res.send("about page");
-});
+const app= require("./src/app.js");
 
 app.listen(3000, () => {
-    console.log(`server is running on port 3000`); //server ko start krne kae liye 3000 is the port number 
-}); 
+    console.log(`server is running on port 3000`);
+});
---
+++ src/app.js
@@ -0,0 +1,10 @@
+// server ko Create krna 
+
+const express = require("express");
+
+const app = express();
+
+module.exports=app;
+
+
+
