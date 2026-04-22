5960272fb20786c8013ef4552cbdb1e0fb652a08
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-22 22:42:56 +0530

    c

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ app.post("/notes",async (req,res)=>{
         title:data.title,
         description:data.description
     })
-    
-    
+
     res.status(201).json({
         success:true,
         message:"note created successfully",
@@ -20,5 +19,53 @@ app.post("/notes",async (req,res)=>{
     })
 });
 
+
+app.get("/notes",async (req,res)=>{
+    const notes = await noteModel.find();
+    res.status(200).json({
+        success:true,
+        message:"notes fetched successfully",
+        data:notes
+    })
+    
+})
+
+// find => this will return the [] or [{},{},{},{},...n times]
+// findOne => this will return the {} or null if not found
+
+
+app.delete("/notes/:id",async (req,res)=>{
+    const id = req.params.id;
+
+    await noteModel.findOneAndDelete({
+        _id:id
+    })
+    res.status(200).json({
+        success:true,
+        message:"note deleted successfully",
+        
+    })
+})
+
+app.patch("/notes/:id",async (req,res)=>{
+    const id = req.params.id;
+    const description=req.body.description;
+    
+
+    const note=await noteModel.findOneAndUpdate(
+        {
+            _id:id
+        },
+        {
+            title:title,
+            description:description
+        },
+        res.status(200).json({
+            success:true,
+            message:"note updated successfully",
+            data:note
+        })
+    )
+})
 
 module.exports = app;
