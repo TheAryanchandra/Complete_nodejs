d3c7ed663f8b1b62c4c3753e0abfc97bf538c720
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-23 11:11:06 +0530

    c

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>hey</h1>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
