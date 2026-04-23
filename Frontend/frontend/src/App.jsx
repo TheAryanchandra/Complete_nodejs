5f464b30b48997bed0a97691d507abf30147962f
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-23 11:28:33 +0530

    s

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
-import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Createpost from './pages/createpost'
 
 function App() {
   return (
