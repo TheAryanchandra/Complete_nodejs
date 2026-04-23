53f92bcf90eb7d58db225a416e7f3f94803c9577
Aryan Chandra <aryanchandra3456@gmail.com>
2026-04-23 11:35:51 +0530

    c

--- Frontend/frontend/src/pages/createpost.jsx
+++ Frontend/frontend/src/pages/createpost.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 const createpost = () => {
   return (
-    <section>
-        <div className="Create-Post">
-        <h1>Create Post</h1>
-        <input type="file" name='image' accept="image/*"/>
-        <input type="text" name="caption" required placeholder='write a caption' />
-        <button type='submit'>Post</button>
+    <section className="createpost">
+        <div className="create-post">
+          <form>
+            <h1>Create Post</h1>
+            <input type="file" name='image' accept="image/*"/>
+            <input type="text" name="caption" required placeholder='write a caption' />
+            <button type='submit'>Post</button>
+          </form>
         </div>
     </section>
   )
